475328ad81f19669bc6588ecec80eed8630b4376
hosein rafiee <robertofantasy@gmail.com>
2025-10-07 09:28:50 +0330

    Initial commit

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/add-product", (req, res, next) => {
-  console.log("First Middleware");
-  res.send(
-    '<form action="/product" method="post" ><input type="text" name="title"><button type="submit">Add product</button></form>'
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>i am the second one</h1>");
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
