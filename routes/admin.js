11797e1e00b6c95dccc1085fd8806ba6a9927170
hosein rafiee <robertofantasy@gmail.com>
2025-10-08 13:39:15 +0330

    add proucts in admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,16 @@ const express = require("express");
 const router = express.Router();
 
 const rootDir = require("../util/path");
+const { title } = require("process");
+
+const products = [];
 
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
