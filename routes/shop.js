47a7551cc80e6abfee7afae2adf9832d3baf6acd
hosein rafiee <robertofantasy@gmail.com>
2025-10-08 09:14:28 +0330

    Updated files and added shop.pug

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const path = require("path");
 
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  //res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  //this code render shop.pug file instead of shop.html
+  res.render("shop", { prods: products, docTitle: "shop" });
 });
 
 module.exports = router;
